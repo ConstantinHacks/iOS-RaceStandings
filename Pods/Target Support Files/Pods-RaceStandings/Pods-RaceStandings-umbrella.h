bab2759903464505d68ada764c71f92d9f8e8643
Constantin Koehler <constantinfakoehler@gmail.com>
2018-01-09 10:22:10 -0500

    Remove old files from refactor

--- Pods/Target Support Files/Pods-RaceStandings/Pods-RaceStandings-umbrella.h
+++ Pods/Target Support Files/Pods-RaceStandings/Pods-RaceStandings-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_RxSwiftFunVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_RxSwiftFunVersionString[];
+FOUNDATION_EXPORT double Pods_RaceStandingsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_RaceStandingsVersionString[];
 
